66368390e574114275e5cbcc58cbec200fe89f77
L.Carquet <laetitiacarquet@gmail.com>
2017-12-27 18:07:56 +0100

    view groups selected and positioned

--- app/src/main/java/com/example/android/quidditchcounter/MainActivity.java
+++ app/src/main/java/com/example/android/quidditchcounter/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quidditchcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayGriff(1);
+    }
+    //these are methods that can display things on the application
+    public void displayGriff(int score){
+        TextView scoreView = (TextView) findViewById(R.id.gryff_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
